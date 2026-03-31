956a6986182b4702a0c6ec9c12d59ef1d9c287f3
Demali-876 <90882773+Demali-876@users.noreply.github.com>
2026-03-30 23:36:14 -0400

    fix: update import

--- server/features/proxy/reverse-proxy.ts
+++ server/features/proxy/reverse-proxy.ts
@@ -1,6 +1,6 @@
 import http  from 'node:http';
 import https from 'node:https';
-import Router from '../../router.ts';
+import Router from '../../router.js';
 
 // ─── Cache ────────────────────────────────────────────────────────────────────
 
